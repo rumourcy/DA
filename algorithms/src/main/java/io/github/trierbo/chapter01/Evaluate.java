2b7956b4a1863d4c6cc0eb1615556fb581ae3f3b
trierbo <baochuanyan@gmail.com>
2019-03-26 17:27:04 +0800

    chapter01 Stack Evaluate

--- algorithms/src/main/java/io/github/trierbo/chapter01/Evaluate.java
+++ algorithms/src/main/java/io/github/trierbo/chapter01/Evaluate.java
@@ -1,5 +1,59 @@
 package io.github.trierbo.chapter01;
 
-public class Evaluate {
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
+public class Evaluate {
+    public static void main(String[] args) {
+        Stack<String> ops = new Stack<>();
+        Stack<Double> vals = new Stack<>();
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            switch (s) {
+                case "+":
+                    ops.push(s);
+                    break;
+                case "-":
+                    ops.push(s);
+                    break;
+                case "*":
+                    ops.push(s);
+                    break;
+                case "/":
+                    ops.push(s);
+                    break;
+                case "sqrt":
+                    ops.push(s);
+                    break;
+                case "(":
+                    break;
+                case ")":
+                    String op = ops.pop();
+                    double val = vals.pop();
+                    switch (op) {
+                        case "+":
+                            val += vals.pop();
+                            break;
+                        case "-":
+                            val -= vals.pop();
+                            break;
+                        case "*":
+                            val *= vals.pop();
+                            break;
+                        case "/":
+                            val /= vals.pop();
+                            break;
+                        case "sqrt":
+                            val = Math.sqrt(val);
+                            break;
+                    }
+                    vals.push(val);
+                    break;
+                default:
+                    vals.push(Double.parseDouble(s));
+            }
+        }
+        StdOut.println(vals.pop());
+    }
 }
